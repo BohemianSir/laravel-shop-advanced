afccf2b7715ff25bcd0e64851667c592c69dd9ce
vagrant <vagrant@homestead>
2019-01-23 12:59:43 +0000

    秒杀商品详情页

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SeckillOrderRequest;
 use App\Http\Requests\CrowdFundingOrderRequest;
 use App\Models\ProductSku;
 use App\Http\Requests\SendReviewRequest;
@@ -151,5 +152,14 @@ class OrdersController extends Controller
         $amount  = $request->input('amount');
 
         return $orderService->crowdfunding($user, $address, $sku, $amount);
+    }
+
+    public function seckill(SeckillOrderRequest $request, OrderService $orderService)
+    {
+        $user    = $request->user();
+        $address = UserAddress::find($request->input('address_id'));
+        $sku     = ProductSku::find($request->input('sku_id'));
+
+        return $orderService->seckill($user, $address, $sku);
     }
 }
